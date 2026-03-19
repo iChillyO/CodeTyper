1fb4c7852dd4bcd1762b8f667335660ff49bc3a3
Sharaf Hazem <sharafhazem123@gmail.com>
2026-03-19 15:26:05 +0200

    chore: Remove `serverExternalPackages` configuration from `next.config.ts`.

--- next.config.ts
+++ next.config.ts
@@ -3,15 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  serverExternalPackages: [
-    "@remotion/bundler",
-    "@remotion/renderer",
-    "@remotion/lambda",
-    "@rspack/core",
-    "@rspack/binding",
-    "@rspack/binding-linux-x64-gnu",
-    "@rspack/binding-linux-arm64-gnu",
-  ]
 };
 
 export default nextConfig;
